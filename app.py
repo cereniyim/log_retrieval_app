8a40a6c5850c8ffa8c1d9f0c8a3a156f65a4837c
cereniyim <iyimceren@gmail.com>
2023-08-27 15:15:36 +0300

    make date also a required parameter art GET endpoint

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from typing import Optional
+from datetime import datetime, timedelta
 
 from flask import Flask
 from flask_restx import Api, Resource, fields
-from datetime import datetime
 
 from mongodb_gateway import MongoDBGateway, NoResultsFound
 
@@ -15,7 +14,7 @@ app = Flask(__name__)
 
 api = Api(app, version="1.0", title="Log API", description="API for retrieving logs")
 
-logs_ns = api.namespace("logs", description="Log operations")
+logs_namespace = api.namespace("logs", description="Log operations")
 
 log_model = api.model(
     "Log",
@@ -29,41 +28,25 @@ log_model = api.model(
 
 parser = api.parser()
 parser.add_argument(
-    "start_date",
-    type=str,
-    help="Filter logs with date greater than or equal to (YYYY-MM-DDTHH:MM:SS)",
-)
-parser.add_argument(
-    "end_date",
-    type=str,
-    help="Filter logs with date less than or equal to (YYYY-MM-DDTHH:MM:SS)",
-)
-parser.add_argument(
     "keyword", type=str, help="Filter logs by keyword, case insensitive"
 )
 
 
-@logs_ns.route("/user/<int:user_id>/level/<string:log_level>")
+@logs_namespace.route("/date/<string:date>/user/<int:user_id>/level/<string:log_level>")
 class LogsResource(Resource):
     @api.doc(parser=parser)
     @api.marshal_list_with(log_model)
     @api.response(200, "Success")
     @api.response(404, "Logs not found")
-    def get(self, user_id: int, log_level: str):
+    def get(self, date: str, user_id: int, log_level: str):
         args = parser.parse_args()
 
-        query = {"user_id": user_id, "log_level": log_level}
-        start_date = args.get("start_date")
-        end_date = args.get("end_date")
-        self._validate_date_format(start_date)
-        self._validate_date_format(end_date)
+        query = {"user_id": user_id, "log_level": log_level, "date": date}
 
-        if start_date:
-            query["date"] = {"$gte": datetime.strptime(start_date, "%Y-%m-%dT%H:%M:%S")}
-        if end_date:
-            if "date" not in query:
-                query["date"] = {}
-            query["date"]["$lte"] = datetime.strptime(end_date, "%Y-%m-%dT%H:%M:%S")
+        date = self._validate_date_format(date)
+        query["date"] = {"$gte": date}
+        query["date"]["$lte"] = date + timedelta(days=1)
+
         if args["keyword"]:
             query["log_message"] = {"$regex": args["keyword"].lower(), "$options": "i"}
 
@@ -78,17 +61,15 @@ class LogsResource(Resource):
             )
         return logs
 
-    def _validate_date_format(
-        self, date: Optional[str], format: str = "%Y-%m-%dT%H:%M:%S"
-    ):
-        if date is not None:
-            try:
-                datetime.strptime(date, format)
-            except ValueError:
-                api.abort(
-                    400,
-                    f"Could not parse date {date}, pass it in YYYY-MM-DDTHH:MM:SS format",
-                )
+    def _validate_date_format(self, date: str, format: str = "%Y-%m-%d") -> datetime:
+        try:
+            log_date = datetime.strptime(date, format)
+            return log_date
+        except ValueError:
+            api.abort(
+                400,
+                f"Could not parse date {date}, pass it in YYYY-MM-DD format",
+            )
 
 
 if __name__ == "__main__":

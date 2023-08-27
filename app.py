6899c176d5ac5e6f3e303bccd12e0ba956e9d16d
cereniyim <iyimceren@gmail.com>
2023-08-27 11:45:21 +0300

    call MongoDBGateway in app.py

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 from flask import Flask
 from flask_restx import Api, Resource, fields
-from pymongo import MongoClient
 from datetime import datetime
 
-app = Flask(__name__)
+from mongodb_gateway import MongoDBGateway, NoResultsFound
 
-# MongoDB connection
-client = MongoClient("mongodb://mongodb:27017/")  # TODO move them to config
-db = client["logs"]
-collection = db["log_collection"]
+app = Flask(__name__)
 
 api = Api(app, version="1.0", title="Log API", description="API for retrieving logs")
 
@@ -36,25 +32,36 @@ parser.add_argument(
     help="Filter logs with date less than or equal to (YYYY-MM-DDTHH:MM:SS)",
 )
 parser.add_argument("user_id", type=int, help="Filter logs by user ID")
-parser.add_argument("log_level", type=str, help="Filter logs by log level, case insensitive")
-parser.add_argument("keyword", type=str, help="Filter logs by keyword, case insensitive")
+parser.add_argument(
+    "log_level", type=str, help="Filter logs by log level, case insensitive"
+)
+parser.add_argument(
+    "keyword", type=str, help="Filter logs by keyword, case insensitive"
+)
 
 
 @logs_ns.route("/")
 class LogsResource(Resource):
     @api.doc(parser=parser)
     @api.marshal_list_with(log_model)
+    @api.response(200, "Success")
+    @api.response(404, "Logs not found")
     def get(self):
+        # TODO validate data here
         args = parser.parse_args()
 
         # Build query based on parameters
         query = {}
         if args["start_date"]:
-            query["date"] = {"$gte": datetime.strptime(args["start_date"], "%Y-%m-%dT%H:%M:%S")}
+            query["date"] = {
+                "$gte": datetime.strptime(args["start_date"], "%Y-%m-%dT%H:%M:%S")
+            }
         if args["end_date"]:
             if "date" not in query:
                 query["date"] = {}
-            query["date"]["$lte"] = datetime.strptime(args["end_date"], "%Y-%m-%dT%H:%M:%S")
+            query["date"]["$lte"] = datetime.strptime(
+                args["end_date"], "%Y-%m-%dT%H:%M:%S"
+            )
         if args["user_id"]:
             query["user_id"] = args["user_id"]
         if args["log_level"]:
@@ -62,16 +69,15 @@ class LogsResource(Resource):
         if args["keyword"]:
             query["log_message"] = {"$regex": args["keyword"].lower(), "$options": "i"}
 
-        # Retrieve logs from MongoDB
-        logs = list(collection.find(query))
+        gateway = MongoDBGateway("mongodb://mongodb:27017/", "logs", "log_collection")
+        # TODO move them to config
 
+        try:
+            logs = gateway.get(query)
+        except NoResultsFound as e:
+            logs_ns.abort(404, e)
         return logs
 
 
 if __name__ == "__main__":
-    # app.run(debug=True)
-    app.run(
-        debug=True,
-        host='0.0.0.0',
-        port=5000
-    )
+    app.run(debug=True, host="0.0.0.0", port=5000)
